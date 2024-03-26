74ab202f9bf1759025abd3c24cbc3dfd40bbdde0
Wright <jason.c.wright@accenture.com>
2024-03-26 16:11:57 -0400

    Adjusting media queries

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function openPopup(url) {
     // Open a new window with the larger image
     window.open(url, "_blank", "width=90vw");
 }
+
+function toggleMenu() {
+    var navMenu = document.getElementById("nav-menu");
+    navMenu.classList.toggle("show");
+}
